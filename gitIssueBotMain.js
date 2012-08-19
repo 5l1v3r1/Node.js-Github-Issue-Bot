992539d52ce764c7ff1706013491c2bddcc345d5
David Clark <dtlark@asu.edu>
2012-08-18 18:59:45 -0700

    Make it watches multiple repos and doesn't announce tickets when starting

--- gitIssueBotMain.js
+++ gitIssueBotMain.js
@@ -1,24 +1,45 @@
 var SETTINGS = {};
 SETTINGS.IRC = {};
 SETTINGS.GIT = {};
+SETTINGS.GIT.user = new Array();
+SETTINGS.GIT.repo = new Array();
 
-// ### Configure Here ### //
-SETTINGS.IRC.channelName = '#firefly';
+// ### CONFIGURE IRC BOT HERE ### //
 SETTINGS.IRC.serverName = 'irc.esper.net';
 SETTINGS.IRC.botName = 'TicketBot';
+SETTINGS.IRC.channelName = '#firefly';
 SETTINGS.IRC.prefix = "New Ticket *** ";
 SETTINGS.IRC.suffix = " ***";
 
-SETTINGS.GIT.user = 'PherricOxide';
-SETTINGS.GIT.repo = 'Node.js-Github-Issue-Bot';
 SETTINGS.GIT.version = '3.0.0';
-
 SETTINGS.pollTime = 5000;
 
+// ### Repositories to announce new issues on ### //
+SETTINGS.GIT.user.push('PherricOxide');
+SETTINGS.GIT.repo.push('Node.js-Github-Issue-Bot');
+
+SETTINGS.GIT.user.push('DataSoft');
+SETTINGS.GIT.repo.push('Nova');
+
+SETTINGS.GIT.user.push('DataSoft');
+SETTINGS.GIT.repo.push('Honeyd');
+
+
+
+
+// ### Check Configuration ### //
+if (SETTINGS.GIT.user.length != SETTINGS.GIT.repo.length) {
+    console.log("Each user must also have a repository set.");
+}
 
 // ### Code ### //
 var irc = require('irc');
 var githubAPI = require('github');
+
+var lastSeenTicket = new Array();
+for (var i = 0; i < SETTINGS.GIT.user.length; i++) {
+    lastSeenTicket[i] = -1; 
+}
 
 var client = new irc.Client(SETTINGS.IRC.serverName, SETTINGS.IRC.botName, {
     port: 6667
@@ -33,25 +54,38 @@ var github = new githubAPI({
     version: SETTINGS.GIT.version
 });
 
-var lastSeenTicket = -1;
 
 
 function poll() {
-    github.issues.repoIssues({
-	user: SETTINGS.GIT.user
-	, repo: SETTINGS.GIT.repo
-	, state: "open"
-	, sort: "created"
-	, page: 0
-	, per_page: 5
-    }, function(err, res) {
-	for (var i = res.length - 1; i >= 0; i--) {
-	    if (res[i].number > lastSeenTicket) {
-		lastSeenTicket = res[i].number;
-		SendIssueToIRC(res[i]);
-	    } 
-	}	
-    });
+    for (var repoIndex = 0; repoIndex < SETTINGS.GIT.user.length; repoIndex++) {
+    (function(repoIndex) {
+	github.issues.repoIssues({
+	    user: SETTINGS.GIT.user[repoIndex]
+	    , repo: SETTINGS.GIT.repo[repoIndex]
+	    , state: "open"
+	    , sort: "created"
+	    , page: 0
+	    , per_page: 5
+	}, function(err, res) {
+	    if (err != null) {
+		console.log("ERROR when polling git for issues");
+		console.log(err);
+	    }
+	   
+	    if (res.length > 0 && lastSeenTicket[repoIndex] < 0) {
+		lastSeenTicket[repoIndex] = res[0].number; 
+	    } else {
+		for (var i = res.length - 1; i >= 0; i--) {
+		    if (res[i].number > lastSeenTicket[repoIndex]) {
+			lastSeenTicket[repoIndex] = res[i].number;
+			SendIssueToIRC(res[i]);
+		    } 
+		}	
+	    }
+
+	});
+    })(repoIndex);
+    }
 }
 
 
@@ -62,12 +96,14 @@ function SendIssueToIRC(issue) {
 
 
 client.addListener("join" + SETTINGS.IRC.channelName, function() {
+    console.log("Joined channel " + SETTINGS.IRC.channelName);
     var timer = setInterval(poll, SETTINGS.pollTime);
+    console.log("Polling github for new issues every " + SETTINGS.pollTime + "ms");
 });
 
 
 client.addListener("error", function(err) {
-    console.log("error event");
+    console.log("Error event!");
     console.log(err);
 });
 
